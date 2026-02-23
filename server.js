b5f847ab7699157ed2390ac62c39ff757e4acb20
Harsh Yadav <harshcode95@gmail.com>
2026-02-24 02:27:40 +0530

    fix: use process.env.PORT for render

--- server.js
+++ server.js
@@ -25,4 +25,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() => {
+  console.log('server running on port ${PORT}');
+});
